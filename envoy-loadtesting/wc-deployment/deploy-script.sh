d90ff99bc7435b9a0e3daf3bfa946a1cd0aff3bc
Zirko <64951262+QuantumEnigmaa@users.noreply.github.com>
2026-03-11 17:48:37 +0100

    update deploy script (#5)

--- envoy-loadtesting/wc-deployment/deploy-script.sh
+++ envoy-loadtesting/wc-deployment/deploy-script.sh
@@ -3,7 +3,7 @@
 AZ="eu-north-1a"
 RELEASE="34.1.0"
 MC="graveler"
-WC="envoytesting"
+WC="envoyloadtesting"
 
 # Configure WC file
 
@@ -12,26 +12,21 @@ sed -i "33s/version: .*/version: ${RELEASE}/g" graveler-loadtesting.yaml
 
 # 1- Create the WC
 echo "Deploying the workload cluster and waiting until it's ready..."
-kubectl apply -f graveler-loadtesting-bis.yaml
+kubectl apply -f graveler-loadtesting.yaml
 
 # 2- Wait for the WC to be ready
-sleep 420
-output=$(kubectl wait --for=condition=Available -n org-giantswarm clusters.cluster.x-k8s.io envoytesting 2>&1)
-if [ $? -ne 0 ]; then
-    echo "Error: Workload cluster did not become available or didn't get generated."
-    echo "Details: $output"
-    exit 1
-fi
+sleep 300
+kubectl wait --for=condition=Available -n org-giantswarm clusters.cluster.x-k8s.io "${WC}"
 echo "Workload cluster is ready."
 
 # 3- Deploy additional apps
 echo "Deploying gateway-api-bundle and aws-lb-controller-bundle..."
-kubectl apply -f graveler-wc-additional-apps-bis.yaml
+kubectl apply -f graveler-wc-additional-apps.yaml
 
 # 4- Wait for the additional apps to be ready
 sleep 60
-kubectl wait --for=jsonpath='{.status.release.status}'=deployed -n org-giantswarm app envoytesting-gateway-api-crds --timeout=300s
-kubectl wait --for=jsonpath='{.status.release.status}'=deployed -n org-giantswarm app envoytesting-gateway-api-config --timeout=1200s
+kubectl wait --for=jsonpath='{.status.release.status}'=deployed -n org-giantswarm app "${WC}"-gateway-api-crds --timeout=300s
+kubectl wait --for=jsonpath='{.status.release.status}'=deployed -n org-giantswarm app "${WC}"-gateway-api-config --timeout=1200s
 echo "gateway-api CRDs deployed and gateway-api-config app is ready."
 
 # 5- log into the WC and deploy the microservice-demo chart
@@ -45,7 +40,7 @@ echo "microservice-demo chart deployed."
 
 # 6- Wait for the the demo-app public endpoint to be reachable
 echo "Waiting for the demo app to be reachable..."
-while ! curl -v https://onlineboutique.loadtesting-0.envoytesting.gaws2.gigantic.io/ | grep "</html>"; do
+while ! curl -v https://onlineboutique.loadtesting-0."${WC}".gaws2.gigantic.io/ | grep "</html>"; do
   sleep 180
 done
 
